5bddcaef0c69615582f1a9f31b32e3ed1b21b02b
ujangaripin24 <ujangarifin06@gmail.com>
2022-12-22 19:42:38 +0700

    2. auth filter has been installed

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -18,11 +18,10 @@ class Filters extends BaseConfig
      * @var array
      */
     public $aliases = [
-        'csrf'          => CSRF::class,
-        'toolbar'       => DebugToolbar::class,
-        'honeypot'      => Honeypot::class,
-        'invalidchars'  => InvalidChars::class,
-        'secureheaders' => SecureHeaders::class,
+        'csrf'     => CSRF::class,
+        'toolbar'  => DebugToolbar::class,
+        'honeypot' => Honeypot::class,
+        'auth'     => App\Filters\Auth::class
     ];
 
     /**
